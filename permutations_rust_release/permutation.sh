b92bf1ec5917492de472d4e1174bea5ad67e98ec
dcc0 <ivangavr777@gmail.com>
2021-03-01 16:52:08 +0300

    Update permutation.sh

--- permutations_rust_release/permutation.sh
+++ permutations_rust_release/permutation.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
-#Скрипт перебора перестановок.
+#Стартовый скрипт для  перебора перестановок.
 #Запуск скрипта из командной строки с аргументом.
 #В качестве первого аргумента: число перестановок.
+#Initial script to generate permutations.
+#We start script out of a command line with an argument.
+#First argument is a number of permutations.
+#MSK(c)2021. E-mail: dcc0@yandex.ru
 
+
+#Argument verifying
 echo "Проверим аргумент";
 if [ !  $1  ];
   then
@@ -10,23 +16,25 @@ echo 'Запустите скрипт с аргументом так: /bin/bash 
 exit;
   fi;
 
+#Проверим файл. File verifying
 if  [ -f ./permutation.rs ]
  then
 rm ./permutation.rs;
   fi;
 
-#Переменные для векторов
+#Переменные для векторов. Vars for vectors
 n=$1;
 y=$1;
 
-#Проверим аргумент на равенство 0
+#Here we are verifying argument equal to 0, so factorial of 0 is 1
+#Проверим аргумент на равенство 0, так факториал 0 равен 1
 if [ "$1" -eq "0" ]
  then
 ((n++));
 ((y++));
  fi;
 
-
+#Here we are creating vectrors
 echo 'Формируем векторы...';
 for (( x=0; x<$n; x++ ))
   do
@@ -35,12 +43,13 @@ VECTOR+="$x,";
 REVERSED_VECTOR+="$y,"
   done
 
-
+#Copying pattern
 echo "Копируем образец";
 cp ./permutation_pattern.rs ./permutation.rs
 sed -i "s/1,0/$REVERSED_VECTOR/g" permutation.rs
 sed -i "s/0,1/$VECTOR/g" permutation.rs
 
+#Compile and Run
 echo "Компилируем и запускаем";
 rustc permutation.rs
 ./permutation
